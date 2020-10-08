74c6d01b20d13a0bdbae5bec78fbd4bb3e92830f
showgayaki <knk.lee.kj@gmail.com>
2020-10-08 14:27:07 +0900

    modified:   backend/settings.py modified:   src/components/TableJpnHoliday.vue

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-if Path.joinpath(BASE_DIR, 'local_settings.py').exists:
+if Path.joinpath(Path.joinpath(Path(__file__).resolve().parent, 'local_settings.py')).exists():
     from .local_settings import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
--- src/components/TableJpnHoliday.vue
+++ src/components/TableJpnHoliday.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="holiday-list">
+  <div class="holiday-wrap">
     <h2>日本の祝祭日</h2>
-    <el-table class="jpn-holiday"
+    <div class="holiday-filter">
+      <p>
+        <label class="filter-label" for="date-filter">date</label>
+        <el-input id="date-filter" class="filter-text" placeholder="Search Date" v-model="date_search" suffix-icon="search"></el-input>
+      </p>
+      <p>
+        <label class="filter-label" for="name-filter">name</label>
+        <el-input id="name-filter" class="filter-text" placeholder="Search Name" v-model="name_search" suffix-icon="search">name</el-input>
+      </p>
+    </div>
+    <p class="view-url">
+      <span>API URL:{{ api_url }}</span>
+      <span v-if=date_search>{{ date_search.replace(date_search, '?date=' + date_search) }}</span>
+      <span v-if=name_search>{{ name_search.replace(name_search, '?name=' + name_search) }}</span>
+    </p>
+    <el-table class="holiday-table"
         :data="holidays"
         :default-sort="{prop: 'date', order: 'ascending'}"
     >
-      <!-- <el-table-column prop="id" label="ID" sortable></el-table-column> -->
       <el-table-column prop="date" label="日付" sortable></el-table-column>
       <el-table-column prop="name" label="名前" sortable></el-table-column>
     </el-table>
@@ -16,9 +30,10 @@
 import Vue from "vue";
 import axios, { AxiosResponse } from "axios";
 
+const API_URL = 'http://127.0.0.1:8000/api/jpn_holiday/';
+
 // API 実行結果
 class JpnHoliday {
-  id: number = 0;
   date: string = "";
   name: string = "";
 }
@@ -26,26 +41,43 @@ class JpnHoliday {
 export default Vue.extend({
   data(){
     return {
+      api_url: API_URL,
       holidays: [new JpnHoliday()],
+      date_search: '',
+      name_search: '',
     };
   },
   created: async function(){
     const res: AxiosResponse = await axios.get(
-        "http://127.0.0.1:8000/api/jpn_holiday/",
+        API_URL,
     );
-    console.log(res.data);
+    // console.log(res.data);
     this.holidays = res.data;
   },
 });
 </script>
 
 <style scoped>
-th.is-leaf{
-  padding: 0;
-}
-.jpn-holiday{
-  width: 50%;
+.holiday-filter{
   margin-top: 1rem;
+}
+.filter-label{
+  display: inline-block;
+  width: 50px;
+}
+.filter-text{
+  margin-bottom: 1rem;
+}
+.filter-text{
+  width: 300px;
+}
+.view-url{
+  margin: 1rem 0;
+}
+.api-url{
+  padding: 0.5rem;
+}
+.holiday-table{
   border: 1px solid rgba(0, 0, 0, .2);
 }
 </style>
