b8cfd48f64f92df96de3d927a46a26a6f30f0fd7
showgayaki <knk.lee.kj@gmail.com>
2020-10-15 23:06:09 +0900

    new file:   .ebextensions/django.config

--- backend/settings.py
+++ backend/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR.joinpath('static')
 
 if DEBUG:
     INSTALLED_APPS += ['corsheaders']
